2819ac041fd0aa1a4960b02b6441e8acd24190c1
Danny Yoo <dansteryoo@gmail.com>
2021-12-25 18:59:32 -0500

    chore: setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,58 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import 'styles/globals.scss';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Off the Pulpit Podcast.</title>
+        <meta name="description" content="Off the Pulpit Podcast." />
+
+        {/* FACEBOOK */}
+        <meta property="og:title" content="Tilt" key="ogTitle" />
+        <meta
+          property="og:description"
+          content="Off the Pulpit Podcast."
+          key="ogDescription"
+        />
+        <meta
+          property="og:image"
+          content="/offThePulpitPod.png"
+          key="ogImage"
+        />
+        <meta
+          property="og:url"
+          content="http://offthepulpitpod.com"
+          key="ogUrl"
+        />
+
+        {/* TWITTER */}
+        <meta name="twitter:title" content="Tilt" key="twitterTitle" />
+        <meta
+          name="twitter:description"
+          content="Off the Pulpit Podcast."
+          key="twitterDescription"
+        />
+        <meta
+          name="twitter:image"
+          content="/offThePulpitPod.png"
+          key="twitterImage"
+        />
+        <meta
+          name="twitter:card"
+          content="/offThePulpitPod.png"
+          key="twitterCard"
+        />
+        <meta
+          property="twitter:url"
+          content="http://offthepulpitpod.com"
+          key="twitterUrl"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
